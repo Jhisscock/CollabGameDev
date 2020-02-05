4f65744d202a77606a0f1881bf655a1d058cefbe
Jhisscock <jthisscock@mail.lipscomb.edu>
2020-02-04 18:48:28 -0600

    eorking on camera controls

---
+++ Prototype/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject topPlayer;
+    public GameObject bottomPlayer;
+    private bool switchSides = true;
+    private Vector3 screenSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenSize = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        mainCamera.cullingMask = ~(1 << 9);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift)){
+            if(switchSides){
+                mainCamera.cullingMask = ~(1 << 8);
+                mainCamera.transform.position = new Vector3((Mathf.Floor(bottomPlayer.transform.position.x/screenSize.x)) * screenSize.x * 2, mainCamera.transform.position.y, mainCamera.transform.position.z);
+                topPlayer.SetActive(false);
+                bottomPlayer.SetActive(true);
+                switchSides = false;
+            }else{
+                mainCamera.cullingMask = ~(1 << 9);
+                Debug.Log(screenSize.x + " : : " + topPlayer.transform.position);
+                mainCamera.transform.position = new Vector3((Mathf.Floor(topPlayer.transform.position.x/screenSize.x)) * screenSize.x * 2, mainCamera.transform.position.y, mainCamera.transform.position.z);
+                topPlayer.SetActive(true);
+                bottomPlayer.SetActive(false);
+                switchSides = true;
+            }
+        }
+        Vector3 screenPosTop = mainCamera.WorldToScreenPoint(topPlayer.transform.position);
+        if(Screen.width - screenPosTop.x <= 50 && PlayerController.topOpen){
+            topPlayer.transform.position += new Vector3(1,0,0);
+            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + screenSize.x * 2, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        }
+
+        Vector3 screenPosBot = mainCamera.WorldToScreenPoint(bottomPlayer.transform.position);
+        if(Screen.width - screenPosBot.x <= 50 && PlayerController.bottomOpen){
+            bottomPlayer.transform.position += new Vector3(1,0,0);
+            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + screenSize.x * 2, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public static bool topOpen = false;
+    public static bool bottomOpen = true;
+    public float speed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(x, y);
+        Walk(dir);
+    }
+
+    void Walk(Vector2 n){
+        rb.velocity = new Vector2(n.x * speed, rb.velocity.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "topOpen"){
+            topOpen = true;
+        }
+        if(other.gameObject.tag == "bottomOpen"){
+            bottomOpen = true;
+        }
+    }
+}
