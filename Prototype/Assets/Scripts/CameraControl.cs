c42246c4fff05a9a6e3c9b100030aea91f75ec0f
Jhisscock <jthisscock@mail.lipscomb.edu>
2020-02-20 15:03:32 -0600

    Screen Works

--- Prototype/Assets/Scripts/CameraControl.cs
+++ Prototype/Assets/Scripts/CameraControl.cs
@@ -9,42 +9,65 @@ public class CameraControl : MonoBehaviour
     public GameObject bottomPlayer;
     private bool switchSides = true;
     private Vector3 screenSize;
+    private int topCount = 0;
+    private int bottomCount = 0;
     // Start is called before the first frame update
     void Start()
     {
         screenSize = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
         mainCamera.cullingMask = ~(1 << 9);
+        bottomPlayer.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //The player switches the view of the camera by masking certain layers and deactivating other players
         if(Input.GetKeyDown(KeyCode.LeftShift)){
             if(switchSides){
                 mainCamera.cullingMask = ~(1 << 8);
-                mainCamera.transform.position = new Vector3((Mathf.Floor(bottomPlayer.transform.position.x/screenSize.x)) * screenSize.x * 2, mainCamera.transform.position.y, mainCamera.transform.position.z);
+                mainCamera.transform.position = new Vector3(screenSize.x * 2 * bottomCount, mainCamera.transform.position.y, mainCamera.transform.position.z);
                 topPlayer.SetActive(false);
                 bottomPlayer.SetActive(true);
                 switchSides = false;
             }else{
                 mainCamera.cullingMask = ~(1 << 9);
-                Debug.Log(screenSize.x + " : : " + topPlayer.transform.position);
-                mainCamera.transform.position = new Vector3((Mathf.Floor(topPlayer.transform.position.x/screenSize.x)) * screenSize.x * 2, mainCamera.transform.position.y, mainCamera.transform.position.z);
+                mainCamera.transform.position = new Vector3(topCount * screenSize.x * 2, mainCamera.transform.position.y, mainCamera.transform.position.z);
                 topPlayer.SetActive(true);
                 bottomPlayer.SetActive(false);
                 switchSides = true;
             }
         }
+
+
+        // Converts player position into a point in pixels relative to the camera view 
+        //and will move the camera if the player gets close to the edge of the screen
         Vector3 screenPosTop = mainCamera.WorldToScreenPoint(topPlayer.transform.position);
-        if(Screen.width - screenPosTop.x <= 50 && PlayerController.topOpen){
+        if(Screen.width - screenPosTop.x <= 50 && Screen.width - screenPosTop.x >= 0 && PlayerController.topOpen){
+            topCount++;
             topPlayer.transform.position += new Vector3(1,0,0);
             mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + screenSize.x * 2, mainCamera.transform.position.y, mainCamera.transform.position.z);
         }
 
         Vector3 screenPosBot = mainCamera.WorldToScreenPoint(bottomPlayer.transform.position);
-        if(Screen.width - screenPosBot.x <= 50 && PlayerController.bottomOpen){
+        if(Screen.width - screenPosBot.x <= 50 &&  Screen.width - screenPosBot.x >= 0 && PlayerController.bottomOpen){
+            bottomCount++;
             bottomPlayer.transform.position += new Vector3(1,0,0);
             mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + screenSize.x * 2, mainCamera.transform.position.y, mainCamera.transform.position.z);
         }
+
+        /*Vector3 screenPosTopLeft = mainCamera.WorldToScreenPoint(topPlayer.transform.position);
+        if(Screen.width - screenPosTopLeft.x >= Screen.width - 50 && Screen.width - screenPosTopLeft.x >= 0){
+            topCount--;
+            topPlayer.transform.position -= new Vector3(1,0,0);
+            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + screenSize.x * -2, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        }
+
+        Vector3 screenPosBotLeft = mainCamera.WorldToScreenPoint(bottomPlayer.transform.position);
+        if(Screen.width - screenPosBotLeft.x >= Screen.width - 50 &&  Screen.width - screenPosBotLeft.x >= 0){
+            bottomCount--;
+            bottomPlayer.transform.position -= new Vector3(1,0,0);
+            mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + screenSize.x * -2, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        }*/
     }
 }
--- Prototype/Assets/Scripts/PlayerController.cs
+++ Prototype/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public static bool topOpen = false;
     public static bool bottomOpen = true;
     public float speed = 4f;
+    public float jHeight = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +22,19 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxis("Vertical");
         Vector2 dir = new Vector2(x, y);
         Walk(dir);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Jump(Vector2.up, false);
+        }
     }
 
     void Walk(Vector2 n){
         rb.velocity = new Vector2(n.x * speed, rb.velocity.y);
+    }
+
+    void Jump(Vector2 n, bool wall){
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += n * jHeight;
     }
 
     void OnTriggerEnter2D(Collider2D other){
