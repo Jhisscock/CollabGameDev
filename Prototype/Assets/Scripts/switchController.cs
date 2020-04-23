7bd1a7ac86ba7cb3ec3c659cb7e0b16dbc252dfd
Samplekt <samplekt@mail.lipscomb.edu>
2020-04-23 17:50:59 -0500

    Graphics Update
    Added character animationa and some sounds

--- Prototype/Assets/Scripts/switchController.cs
+++ Prototype/Assets/Scripts/switchController.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class switchController : MonoBehaviour
 {
     public GameObject gameManager;
+    public AudioSource switchSound;
     private CameraControl CC;
     private Room_1 R_1;
     // Start is called before the first frame update
     void Start(){
         CC = gameManager.GetComponent<CameraControl>();
         R_1 = gameManager.GetComponent<Room_1>();
+        switchSound = gameManager.GetComponent<AudioSource>();
     }
     void OnTriggerStay2D(Collider2D other){
         if(other.gameObject.tag == "Player" && Input.GetKey(KeyCode.RightShift)){
             R_1.Room_1_Counter();
             CC.SwitchScreens();
+            switchSound.Play();
         }
     }
 
-    
+
 }
